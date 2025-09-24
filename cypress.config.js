c479d945559debae9aefed7add168ffb8576a785
Rubens Lima <rubens.souzalima@hotmail.com>
2025-09-24 20:02:37 -0300

    Automação com cypress utilizando hooks e configurando a geração de relatórios em HTML e XML

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  video: true,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'cypress/reports/junit/results-[hash].xml'
+    },
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: 'Relatório de testes',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false // aqui depende se faz sentido salvar os reteste caso de algum erro no seu teste
+    }
+  },
+  chromeWebSecurity: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
+    },
+  },
+});
